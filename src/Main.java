2d7a0b46f4dbf974d7f9dbd4743137e918ba254c
Esmeralda Muñoz <mf23004@ues.edu.sv>
2025-08-26 23:15:37 -0600

    Update Main.java
    Cambios en mi coche

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,20 @@ public class Main {
       
         // Crear un objeto de tipo Coche
 
-
+coche cocheali = new Coche ("Toyota", "Supra", "Amarillo");
          // Mostrar información inicial del coche
-
+    System.out.println("Marca: " + cocheali.getMarca());
+    System.out.println("Modelo: " + cocheali.getModelo());
+    System.out.println("Color: " + cocheali.getColor());
+    System.out.println("¿Está encendido? " + cocheali.isEncendido());
         // Encender el coche
-
+    cocheali.arrancar();
                 // Cambiar el color del coche
-
+        cocheali.setColor("Azul");
+        System.out.println("Nuevo color del coche: " + cocheali.getColor());
+        
                 // Apagar el coche
-     
+     cocheali.apagar();
         }
     }
 }
