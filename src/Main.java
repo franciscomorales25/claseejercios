71ed8b716d6007fcd808db216a41534c70a70a89
JeffersonRamos-rd24003 <rd24003@ues.edu.sv>
2025-08-26 20:15:50 -0600

    Creación y manipulación de instancia de Coche

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-      
-        // Crear un objeto de tipo Coche
 
+        // Crear un objeto de tipo Coche
+        Coche coche = New Coche("Toyota", "123", "Rojo");
 
          // Mostrar información inicial del coche
+        coche.getMarca(); coche.getModelo(); coche.getColor();
 
         // Encender el coche
+        coche.arrancar();
 
                 // Cambiar el color del coche
+                coche.setColor("Azul");
 
                 // Apagar el coche
+                coche.apagar();
      
         }
     }
