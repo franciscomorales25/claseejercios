090cc27b3b7b39dea404468b17413e759f805e14
cindy120307 <rm24001@ues.edu.sv>
2025-09-02 15:17:09 -0600

    Actualizacion funcional de programa de java coche

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,34 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-      
+        Scanner sc = new Scanner(System.in);
+
         // Crear un objeto de tipo Coche
+        Coche miCoche = new Coche("Toyota", "Corolla", "Rojo");
 
-
-         // Mostrar información inicial del coche
+        // Mostrar información inicial del coche
+        System.out.println("Marca: " + miCoche.getMarca());
+        System.out.println("Modelo: " + miCoche.getModelo());
+        System.out.println("Color: " + miCoche.getColor());
+        System.out.println("¿Está encendido el coche? " + miCoche.isEncendido());
 
         // Encender el coche
+        miCoche.arrancar();
+        System.out.println("¿Está encendido? " + miCoche.isEncendido());
 
-                // Cambiar el color del coche
+        // Cambiar el color del coche
+        System.out.print("Ingrese el nuevo color del coche porfi: ");
+        String nuevoColor = sc.nextLine();
+        miCoche.setColor(nuevoColor);
+        System.out.println("El nuevo color es : " + miCoche.getColor());
 
-                // Apagar el coche
-     
-        }
+        // Apagar el coche
+        miCoche.apagar();
+        System.out.println("¿Está encendido el coche? " + miCoche.isEncendido());
+
+        sc.close();
     }
 }
