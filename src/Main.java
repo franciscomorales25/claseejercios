c53f918247accd81c5b7bd95c7bab73f04412281
Stanly <rg24040@ues.edu.sv>
2025-09-02 10:33:25 -0600

    RG24040: se implementa creación de coche, encendido, cambio de color y apagado

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-      
+
         // Crear un objeto de tipo Coche
+        Coche c1 = new Coche("Mitsubishi", "Lancer", "Azul");
 
+        // Mostrar información inicial del coche
+        System.out.println("\n Marca: " + c1.getMarca() + "\n MOdelo: " + c1.getModelo() + "\n Color: " + c1.getColor());
 
-         // Mostrar información inicial del coche
 
         // Encender el coche
+        c1.arrancar();
 
-                // Cambiar el color del coche
+        // Cambiar el color del coche
+        c1.setColor("Rojo");
+        System.out.println("El nuevo color del coche es: "+c1.getColor());
 
-                // Apagar el coche
-     
-        }
+        // Apagar el coche
+        c1.apagar();
     }
 }
+
+
